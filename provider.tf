7f41a60c462c9f11b96f43f6ffda39f86c1266ca
Calvin Cunningham <105311977+calvinjrcun77@users.noreply.github.com>
2023-04-12 20:16:50 -0700

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,8 +8,8 @@ terraform {
 }
 
 provider "azurerm" {
-  subscription_id = "0490d8dd-39e8-4fa9-80c3-898f049e3b6d"
-  client_id       = "5e3ecc85-00bb-4c07-8e49-dc8fda2c8cd4"
-  client_secret   = "JiO8Q~dfPlcwdVCI6jHjwyK_fNbm15umzADf8baP"
-  tenant_id       = "6bd18ffc-7a23-4624-904f-045843b60843"
+  subscription_id = 
+  client_id       = 
+  client_secret   = 
+  tenant_id       = 
 }
